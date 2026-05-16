a42ba0f36c38422fa45266ca2e3b4cec6b60725f
ManhThang06 <manhthang26032006@gmail.com>
2026-05-16 15:07:59 +0700

    commit

--- resources/js/db.js
+++ resources/js/db.js
@@ -6,15 +6,3 @@ db.version(1).stores({
     notes: '++id, server_id, title, content, updated_at, sync_status', // sync_status: 'synced', 'pending_create', 'pending_update', 'pending_delete'
     labels: '++id, server_id, name'
 });
-
-export const saveNoteLocally = async (note) => {
-    return await db.notes.put({
-        ...note,
-        server_id: note.id,
-        sync_status: 'synced'
-    });
-};
-
-export const getLocalNotes = async () => {
-    return await db.notes.toArray();
-};
